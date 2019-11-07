ef40bd5139e2743e6f11508aa27a2e2faba70a7b
yang <yang@qq.com>
2019-11-07 09:58:51 +0800

    第二次提交项目

--- src/main/java/com/bw/main/Main.java
+++ src/main/java/com/bw/main/Main.java
@@ -3,6 +3,8 @@ package com.bw.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("helloWord");
+		System.out.println("helloJava");
+		System.out.println("wocao");
+		System.out.println("woqing");
 	}
 }
